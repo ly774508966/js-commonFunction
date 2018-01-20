a153d7350f6e4912aeadf58cfbaa1afee65b2b77
郝赵杰(413121) <413121@nd.com>
2018-01-20 17:12:06 +0800

    更新cookie函数

--- src/cookie/cookie.js
+++ src/cookie/cookie.js
@@ -1,21 +1,30 @@
 //关于cookie操作的一些函数
 
-//获取cookie值
-function getCookie (cookieName) {
-    var obj = {};
-    var cookie = document.cookie.split(';'), len = cookie.length;
-    for(var i = 0;i < len; i++){
-    	var arr = cookie[i].trim().split('=');
-    	obj[arr[0]] = arr[1];
+//设置cookie
+function setCookie(key,value,expires,domain,path) {
+    var encodeKey = encodeURIComponent(key);
+    var encodeValue = encodeURIComponent(value);
+    var keyExpires = expires.toUTCString();
+    document.cookie = encodeKey + '=' + encodeValue + ';expires=' + keyExpires + ';domain=' + domain + ';path=' + path;
+}
+//获取cookie
+function getCookie(key){
+    var encodeKey = encodeURIComponent(key);
+    var allCookie = document.cookie.split(';');
+    var cookie,len = allCookie.length;
+    for (var i = 0; i < len; i++) {
+        cookie = allCookie[i].split('=');
+        if (encodeKey == cookie[0]) {
+            return cookie[1];
+        }   
     }
-    if(cookieName in obj)
-    	return obj[cookieName];
-    else {
-        return 'cookie中无该值';
-    }
+    return '';
 }
 
-//设置cookie值
-function setCookie (cookieName, cookieValue) {
-    document.cookie = encodeURIComponent(cookieName) + '=' + encodeURIComponent(cookieValue);
+//删除cookie
+function delCookie(key,domain,path){
+    var encodeKey = encodeURIComponent(key);
+    var expires = new Date(0);
+    document.cookie = encodeKey + '=' + ';expires=' + expires.toUTCString() + ';domain=' + domain + ';path=' + path;
 }
+  
